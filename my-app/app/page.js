961bdc485d8d482492305c1e28ad3d18b74ff560
Sravan T P <sravantp03@gmail.com>
2023-03-05 11:33:35 +0530

    fixed wallet connect

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,21 +4,36 @@ import Web3Modal from "web3modal";
 import styles from "./page.module.css";
 import { useState, useEffect, useRef } from "react";
 import { ethers } from "ethers";
+import contractInfo from "../contract/contract.json";
 
 export default function Home() {
   const web3modalRef = useRef();
   const [walletConnected, setWalletConnected] = useState(false);
 
   const connectWallet = async () => {
-    const instance = await web3modalRef.current.connect();
-    const provider = new ethers.providers.Web3Provider(instance);
-    const signer = provider.getSigner();
-    return signer;
+    try {
+      const instance = await web3modalRef.current.connect();
+      const provider = new ethers.providers.Web3Provider(instance);
+      const signer = provider.getSigner();
+
+      // contract instance
+      const contract = new ethers.Contract(
+        contractInfo.address,
+        contractInfo.abi,
+        signer
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   const handleClick = async () => {
-    await connectWallet();
-    setWalletConnected(true);
+    try {
+      await connectWallet();
+      setWalletConnected(true);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   useEffect(() => {
